6bdd6f3ba245962d55ceefefc879c3e6a40dc33e
kudeh <kene.udeh@gmail.com>
2020-05-25 21:09:29 -0400

    created collection component and set up routing to collection

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,9 +2,25 @@ import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
-const selectShopCollections = createSelector(
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    mens: 5
+}
+
+export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
 
-export default selectShopCollections;
+export const selectCollectionsForPreview = createSelector(
+    [selectShopCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectShopCollections], 
+    collections => collections[collectionUrlParam]
+)
