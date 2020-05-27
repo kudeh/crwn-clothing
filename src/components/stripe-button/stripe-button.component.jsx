b519e0f52676da65393375bba4c4c1b03bbac489
kudeh <kene.udeh@gmail.com>
2020-05-27 10:28:33 -0400

    integrated test stripe payment to applications

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_qyEdZKSDLn8WL7903LhIGK9000Rn8M1ah5";
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payement Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
