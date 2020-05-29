d7956d5edfcd65bbdfc7780dbdf8c343ce4d0d53
kudeh <kene.udeh@gmail.com>
2020-05-29 00:19:47 -0400

    fixed styling issues

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { CustomButtonContainer } from './custom-button.styles';
 
+import './custom-button.styles.scss';
 
 const CustomButton = ({children, ...props}) => (
     <CustomButtonContainer {...props} >
