57244fcd956ff06f322d1d09061f7284934dd179
kudeh <kene.udeh@gmail.com>
2020-05-30 13:02:13 -0400

    moved shop data from redux to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -56,6 +56,27 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+    
+    const transformedCollections = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title: title,
+            items: items
+        }
+    })
+
+    return transformedCollections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
+
+
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
